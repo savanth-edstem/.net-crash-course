e7264d157acaf0c458beefd80de326e1139b8fbb
savanth-edstem <savanth.s@edstem.com>
2023-01-11 12:50:32 +0530

    day 2 assignment corrected

--- Day2Assmt1ATM/Program.cs
+++ Day2Assmt1ATM/Program.cs
@@ -1,98 +1,120 @@
-﻿int cardNnumber = 56789;
-int pinNumber = 1234;
-int balance = 5000;
+﻿int balance = 5000;
 string run = string.Empty;
-try
+bool verify = false;
+
+bool Verification()
 {
-    do
+    int cardNnumber = 56789;
+    int pinNumber = 1234;
+    Console.Write("Enter the card number: ");
+    int newCardNumber = Convert.ToInt32(Console.ReadLine());
+    if (newCardNumber == cardNnumber)
     {
-        AtmMachine();
-        Console.WriteLine("press any key to continue or \"e\" to exit ");
-        run = Console.ReadLine();
-
-
-        void AtmMachine()
+        Console.Write("Enter the pin number: ");
+        int newPinNumber = Convert.ToInt32(Console.ReadLine());
+        if (newPinNumber == pinNumber)
         {
-            Console.Write("Enter the card number: ");
-            int newCardNumber = Convert.ToInt32(Console.ReadLine());
-            if (newCardNumber == cardNnumber)
-            {
-                Console.Write("Enter the pin number: ");
-                int newPinNumber = Convert.ToInt32(Console.ReadLine());
-                if (newPinNumber == pinNumber)
-                {
-                    MenuScreen();
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect Entry!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Incorrect Entry!");
-            }
+            return true;
         }
-
-
-        void MenuScreen()
+        else
         {
-            MenuOptions('b', "Balance");
-            MenuOptions('d', "Deposit");
-            MenuOptions('w', "Withdraw");
-            MenuOptions('q', "Quit");
-            char menuOption = Convert.ToChar(Console.ReadLine());
-            switch (menuOption)
-            {
-                case 'b':
-                    Console.WriteLine($"Your balance is {balance}");
-                    break;
-                case 'd':
-                    Deposit();
-                    break;
-                case 'w':
-                    Withdraw();
-                    break;
-                case 'q':
-                    break;
-                default:
-                    Console.WriteLine("Incorrect entry or no option selected");
-                    Console.WriteLine("Try again");
-                    break;
-
-            }
-        }
-
-
-        void MenuOptions(char character, string option)
-        {
-            Console.WriteLine($"Press {character} to {option}");
-        }
-
-
-        void Deposit()
-        {
-            Console.Write("Enter the amount to deposit: ");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            balance += amount;
-            Console.WriteLine("Successful");
-            Console.WriteLine($"Your new balance: {balance}");
-        }
-
-        void Withdraw()
-        {
-            Console.Write("Enter the amount to withdraw: ");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            balance -= amount;
-            Console.WriteLine("Successful");
-            Console.WriteLine($"Your new balance: {balance}");
+            return false;
         }
     }
-    while (!run.Equals("e"));
+    else
+    {
+        return false;
+    }
+}
+
+
+void MenuScreen()
+{
+    Console.WriteLine();
+    MenuOptions('b', "Balance");
+    MenuOptions('d', "Deposit");
+    MenuOptions('w', "Withdraw");
+    MenuOptions('q', "Quit");
+    char menuOption = Convert.ToChar(Console.ReadLine());
+    switch (menuOption)
+    {
+        case 'b':
+            Balance();
+            break;
+        case 'd':
+            Deposit();
+            break;
+        case 'w':
+            Withdraw();
+            break;
+        case 'q':
+            run = "e";
+            break;
+        default:
+            UnknownMenu();
+            break;
+
+    }
+}
+
+
+void MenuOptions(char character, string option)
+{
+    Console.WriteLine($"Press {character} to {option}");
+}
+
+
+void Balance()
+{
+    Console.WriteLine($"Your balance is {balance}");
+}
+
+
+void UnknownMenu()
+{
+    Console.WriteLine("Incorrect entry or no option selected");
+    Console.WriteLine("Try again");
+}
+
+void Deposit()
+{
+    Console.Write("Enter the amount to deposit: ");
+    int amount = Convert.ToInt32(Console.ReadLine());
+    balance += amount;
+    Console.WriteLine("Successful");
+    Console.WriteLine($"Your new balance: {balance}");
+}
+
+void Withdraw()
+{
+    Console.Write("Enter the amount to withdraw: ");
+    int amount = Convert.ToInt32(Console.ReadLine());
+    balance -= amount;
+    Console.WriteLine("Successful");
+    Console.WriteLine($"Your new balance: {balance}");
+}
+
+try
+{
+
+    if (Verification() == true)
+    {
+        do
+        {
+            MenuScreen();
+        }
+        while (!run.Equals("e"));
+    }
+    else
+    {
+        Console.WriteLine("Unable to verify credentials!");
+    }
+
+
 }
 catch (FormatException)
 {
-    Console.WriteLine($"invalid entry");
+    Console.WriteLine("invalid entry!!!");
 }
 catch (Exception)
 {
